e297c78a4e5ecf1f604058b81c12c2bf68c72c79
Shelm8 <80206268+IUE-LMS@users.noreply.github.com>
2024-01-17 15:10:55 +0000

    Commit 1 - base project with simple UI

---
+++ README.md
@@ -0,0 +1,28 @@
+# vue-app
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Images
+Favicon by Mayor Icons https://www.freepik.com/icon/globe_11984866
+Postbox.jpeg by 
---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+ 
+    <Welcome msg="Welcome to Geo Info"/>
+
+  <!-- <img alt="Free stock image by Lina Kivaka" src="./assets/Postbox.jpeg"> -->
+      
+    <form>
+      <p><b>Postcode: </b></p>
+      <p><input type="text" required v-model="postcode" placeholder="e.g. BT41 1XP"></p>
+      <button type="submit">Search</button>    
+    </form>
+        
+  </div>
+</template>
+
+<script>
+
+import Welcome from './components/Welcome.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Welcome
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 10px;
+  background-color: #F2F1EB;
+ 
+  
+
+}
+</style>
